12a4dd652e65c2d4130e143783cc852b22a6d6e0
xiaodingyang <346629678@qq.com>
2021-01-26 10:30:32 +0800

    能够使用版本更新

--- examples/index.js
+++ examples/index.js
@@ -1,23 +1,29 @@
-/*********************在examples文件夹中新建index.js用来整合所有的组件，形成组件库对外导出。*******************/
-
-import xtable from '../packages/x-table/index.js'
-import xedit from '../packages/x-edit/index.js'
+import Edit from '../packages/x-edit/index.js'
+import Table from '../packages/x-table/index.js'
 
 // 存储组件列表
-const components = [xtable,xedit]
+const components = [
+    Edit,
+    Table
+]
 
-const install = {
-    // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
-	install: function(Vue) {
-        // 判断是否安装
-		 return components.map((component) => Vue.component(component.name, component)) // 遍历注册全局组件
-	},
+// 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
+const install = function (Vue) {
+// 判断是否安装
+if (install.installed) return
+// 遍历注册全局组件
+components.map(component => Vue.component(component.name, component))
 }
 
 // 判断是否是直接引入文件
 if (typeof window !== 'undefined' && window.Vue) {
-	install.install(window.Vue)
+install(window.Vue)
 }
-export const table = xtable // 按需引入
-export const edit = xedit // 按需引入
-export default install // 加载全部组件
+
+export default {
+// 导出的对象必须具有 install，才能被 Vue.use() 方法安装
+install,
+// 以下是具体的组件列表
+Edit,
+Table
+}
